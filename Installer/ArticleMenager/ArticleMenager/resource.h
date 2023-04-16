e11a9064d6597ee1e3b830013e7fb0428a4c6a56
Primo84 <80720100+Primo84@users.noreply.github.com>
2023-04-16 20:06:57 +0200

    Add files via upload

---
+++ Installer/ArticleMenager/ArticleMenager/resource.h
@@ -0,0 +1,63 @@
+
+#include "windows.h"
+
+//bitmapy
+
+#define IDB_1 100
+#define IDB_2 101
+#define IDB_3 102
+#define IDB_4 103
+#define IDB_5 104
+
+//ikony
+
+#define IDI_1 200
+
+//przycisk Dodaj, Anuluj
+
+#define DLG_B1		300
+#define DLG_B2		301
+#define DLG_B11		302
+#define DLG_B33		303
+#define DLG_B4		304
+#define DLG_B111	305
+#define DLG_B222	306
+#define DLG_B333	307
+
+// Edit control, progress,text
+#define DLG_E1 308
+#define DLG_PB 309
+#define DLG_T1 310
+
+#define ID_FILE1	500
+#define ID_FILE2	501
+#define ID_FILE3	502
+#define ID_FILE4	503
+#define ID_FILE5	504
+#define ID_FILE6	505
+#define ID_FILE7	506
+#define ID_FILE8	507
+#define ID_FILE9	508
+#define ID_FILE10	509
+#define ID_FILE11	510
+#define ID_FILE12	511
+
+
+// Strintable
+
+#define FILE1	550
+#define FILE2	551
+#define FILE3	552
+#define FILE4	553
+#define FILE5	554
+#define FILE6	555
+#define FILE7	556
+#define FILE8	557
+#define FILE9	558
+#define FILE10	559
+#define FILE11	560
+#define FILE12	561
+//Okno dialogowe
+
+#define ID_DLG_1	600
+#define DLG1_ST1	601
